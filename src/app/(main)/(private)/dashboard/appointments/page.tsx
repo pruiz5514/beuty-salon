8565caa77c3a371141367bea3893ca8192add28a
Pablo Ruiz <pablo_ruiza@hotmail.com>
2024-11-11 23:00:30 -0500

    add log out function

--- src/app/(main)/(private)/dashboard/appointments/page.tsx
+++ src/app/(main)/(private)/dashboard/appointments/page.tsx
@@ -1,6 +1,6 @@
-// import { EmployeesService } from "@/app/infrastructure/services/employees.service";
-// import EmployeesTable from "@/components/organisms/Tables/EmployeesTable";
-// import FeatureTemplate from "@/components/template/private/FeatureTemplate/FeatureTemplate"
+import { EmployeesService } from "@/app/infrastructure/services/employees.service";
+import EmployeesTable from "@/components/organisms/Tables/EmployeesTable";
+import FeatureTemplate from "@/components/template/private/FeatureTemplate/FeatureTemplate"
 
 // const useAppointmentsService = new AppointmentsService();
 
@@ -12,19 +12,20 @@
 //   }
 // }
 
-// export default async function AppointmentsPage({searchParams}:IProps) {
+export default async function AppointmentsPage({searchParams}:IProps) {
 //   const page = searchParams.page ? parseInt(searchParams.page) : 1;
 //   const size = searchParams.size ? parseInt(searchParams.size) : 8;
 //   const order = searchParams.order || ''
 
 //   const employees = await useAppointmentsService.findAllAppointments(`employees?page=${page}&size=${size}`,{order});
   
-//   return (
-//     <main>
-//       <FeatureTemplate data={employees} title='Citas' feature="appointments">
-//         <AppointmentsTable employees={employees.content}/>
-//       </FeatureTemplate>
-//     </main>
+  return (
+    <main>
+        <h1>Citas</h1>
+      {/* <FeatureTemplate data={employees} title='Citas' feature="appointments">
+        <AppointmentsTable employees={employees.content}/>
+      </FeatureTemplate> */}
+    </main>
     
-//   )
-// }
+  )
+}
